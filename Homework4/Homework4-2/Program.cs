d458b1e48d72cde965d0a8791d30abb83bf5755d
IzumiSakai <542270191@qq.com>
2020-03-12 09:03:00 +0800

    这是第四次C#作业

---
+++ Homework4/Homework4-2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Homework4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = 3;
+            int minute = 0;
+            int second = 0;
+            Form form = new Form();
+            while (true)
+            {
+                System.Threading.Thread.Sleep(2000);
+                form.clockEvent.TickOrAlarm(hour, minute, second);
+                hour++;
+            }
+        }
+    }
+    public delegate void ClockHandler(Object seder,EventArgs args);
+    public class EventArgs
+    {
+        public int hour { get; set; }
+        public int minute { get; set; }
+        public int second { get; set; }
+    }
+    public class ClockEvent
+    {
+        public event ClockHandler handler;
+        public void TickOrAlarm(int hour,int minute,int second)
+        {
+            EventArgs args = new EventArgs { hour = hour, minute = minute, second = second };
+            handler(this, args);
+        }
+    }
+    public class Form
+    {
+        public ClockEvent clockEvent = new ClockEvent() ;
+        public Form()
+        {
+            clockEvent.handler += tick;
+            clockEvent.handler += alarm;
+        }
+        public void tick(Object sender,EventArgs args)
+        {
+            Console.WriteLine("滴答滴答");
+        }
+        public void alarm(Object sender,EventArgs args)
+        {
+            if (args.hour == 8 && args.minute == 0)
+            {
+                Console.Write("上早八啦，上早八啦       ");
+                Console.WriteLine("闹铃响啦闹铃响啦");
+            }
+        }
+    }
+}
---
+++ Homework4/Homework4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework4
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyList<int> myList = new MyList<int>();
+            myList.AddFirst(313);
+            myList.AddAfter(myList.First,4324);
+            myList.AddAfter(myList.First,767);
+            myList.ForEach(x => Console.WriteLine(x));
+        }
+    }
+    /*public class Node<T>
+    {
+        public Node<T> next { get; set; }
+        public T data { get; set; }
+        public Node() { }
+        public Node(T t) { this.data = t; }
+    }*/
+    public class MyList<T> : System.Collections.Generic.LinkedList<T>
+    {
+        new public void ForEach(Action<LinkedListNode<T>> action)
+        {
+            LinkedListNode<T> node = this.First;
+            while (node!=null)
+            {
+                action(node);
+                node = node.Next;
+            }
+        }
+    }
+}
